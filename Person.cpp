617f94555a9a6d8419a79b9c50b71d28361c0c90
Mahdi <mahdi.bani.2003@gmail.com>
2022-06-06 22:58:23 +0400

    Class Person created

---
+++ Person.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mahdi on 6/6/2022.
+//
+
+#include "Person.h"
---
+++ Person.h
@@ -0,0 +1,20 @@
+
+#ifndef TAMRIN5_PERSON_H
+#define TAMRIN5_PERSON_H
+
+#include <string.h>
+#include <iostream>
+
+class Person {
+
+private:
+    std::string firstName,lastName,id;
+    double* workHours;
+
+
+
+
+};
+
+
+#endif //TAMRIN5_PERSON_H
