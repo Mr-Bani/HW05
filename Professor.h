6df1347322b020bf7789603f6808394e02cd5629
Mahdi <mahdi.bani.2003@gmail.com>
2022-06-12 19:53:51 +0400

    Professor created

---
+++ Professor.h
@@ -0,0 +1,19 @@
+//
+// Created by Mahdi on 6/12/2022.
+//
+
+#ifndef TAMRIN5_PROFESSOR_H
+#define TAMRIN5_PROFESSOR_H
+
+#include "Person.h"
+#include "string"
+
+class Professor : public Person {
+
+private:
+    std::string title;
+
+};
+
+
+#endif //TAMRIN5_PROFESSOR_H
